ddf454f86699f917f87851e8a49d5d8146e12e72
Shriya Upadhyay <shriyaup@usc.edu>
2023-03-30 07:00:22 +0000

    added comments

--- schedwork.cpp
+++ schedwork.cpp
@@ -38,62 +38,76 @@ bool schedule(
     }
     sched.clear();
 
-		//keep track of n (days) and k ()
+	//keep track of n (days) and k ()
     size_t n = avail.size();
     size_t k = avail[0].size();
 
 
     std::map<unsigned int, int> usedWorkers;
 
+        //initalize map to keep track of worker ids and how many shifts they have worked
 		for (unsigned int i = 0; i < k; i++) {
 			usedWorkers[i] = 0;
 		}
 
+        //initalize schedule with all -1 instead of possible worker ids
         for (unsigned int i = 0; i < n; i++ ){
             std::vector<unsigned int> k (dailyNeed, -1);
             sched.push_back(k);
         }
 
     return schedulehelper(0, 0, dailyNeed, n, k, maxShifts, usedWorkers, avail, sched);
-    // Add your code below
 
 }
 
 bool schedulehelper(size_t row, size_t col, size_t d, size_t n, size_t k, const size_t maxShifts, std::map<unsigned int, int> usedWorkers, const AvailabilityMatrix& avail, DailySchedule& sched) {
+    //return if we have reached the end, aka a path has been found
     if (row == n) {
         return true;
     }
-        for (Worker_T i = 0; i < k; i++) {
-            sched[row][col] = i;
-            usedWorkers[i]++;
-            if (isValid(row, col, i, k, maxShifts, usedWorkers, avail, sched)) {
-                if (col + 1 == d ) {
-                    if (schedulehelper(row + 1, 0, d, n, k, maxShifts, usedWorkers, avail, sched)) {
-                        return true;
-                    }
-                }
 
-                else {
-                    if (schedulehelper(row, col+1, d, n, k, maxShifts, usedWorkers, avail, sched) ) {
-                        return true;
-                    }
+    //loop through possible worker ids and assign worker id to current spot and increment the number of shifts that worker has worked in the map
+    for (Worker_T i = 0; i < k; i++) {
+        sched[row][col] = i;
+        usedWorkers[i]++;
+        //if the current placing is valid, recurse
+        if (isValid(row, col, i, k, maxShifts, usedWorkers, avail, sched)) {
+            //recurse to next row if in last column of a row
+            if (col + 1 == d ) {
+                if (schedulehelper(row + 1, 0, d, n, k, maxShifts, usedWorkers, avail, sched)) {
+                    return true;
                 }
             }
 
+            //recurse to next column in row
             else {
-                usedWorkers[i]--;
+                if (schedulehelper(row, col+1, d, n, k, maxShifts, usedWorkers, avail, sched) ) {
+                    return true;
+                }
             }
         }
+
+        //if invalid, decrement the number of shifts worked
+        else {
+            usedWorkers[i]--;
+        }
+    }
+
+    //if we reach the end of possible values without finding a valid solution, return false and reset the value of the current spot
         sched[row][col] = -1;
         return false;
 
 }
 
+//checks if current worker being placed in current spot is valid
+
 bool isValid(size_t row, size_t col, size_t i, size_t k, const size_t maxShifts, std::map<unsigned int, int> usedWorkers, const AvailabilityMatrix& avail, DailySchedule& sched) {
+    //return false if the current worker id is invalid
     if (i == INVALID_ID) {
         return false;
     }
     
+    //return false if the worker is not available during the current time or if the worker has worked more than the maxshifts already
     unsigned int shifts = usedWorkers[i];
     if (!(avail[row][i]) || shifts > maxShifts) {
         return false;
--- wordle.cpp
+++ wordle.cpp
@@ -45,58 +45,71 @@ std::set<std::string> wordle(
 void wordle_helper(std::string& current, unsigned int current_ind, std::queue<char>& floatingChars, std::set<std::string>& words, const std::set<std::string>& dict, unsigned int num_blanks)
 {
 
+//create set of characters to keep track of used_letters;
 std::set<char> used_letters; 
     
+    //if the current letter we are on, is greater than the length of the word, the word is full
+    //check if the word is in dictionary and add if so
     if (current_ind >= current.size()) {
         if (dict.find(current)!= dict.end()) {
             words.insert(current);  
         }
+        //reset the current index to 0 and return
         current_ind = 0;
         return;
 	} 
 
 
-else {
-  int ascii = 97;
-    if(current[current_ind] == '-'){
-        int n = floatingChars.size();
-        for(int i = 0; i < n; i++) {
-            char temp = floatingChars.front();
-            if ( used_letters.find(temp) != used_letters.end()) {
-                continue;
-            }
-            current[current_ind] = temp;
-            if (num_blanks > 0 ){
-             num_blanks--;
-             }
-            used_letters.insert(temp);
-            floatingChars.pop();
-            wordle_helper(current, current_ind + 1, floatingChars, words, dict, num_blanks);
-            current[current_ind] = '-';
-            num_blanks++;
-            floatingChars.push(temp);
-        }
-
-
-
-        if (num_blanks > floatingChars.size()) {
-        while (ascii < 123) {
-            if (used_letters.find(ascii) != used_letters.end()) {
-                ascii++;
+    else {
+        int ascii = 97;
+        //if the current index is a blank -, loop through the possible letter options
+        if(current[current_ind] == '-'){
+            //first loop through floating characters, pushing each character to used letters through each iteration
+            int n = floatingChars.size();
+            for(int i = 0; i < n; i++) {
+                char temp = floatingChars.front();
+                if (used_letters.find(temp) != used_letters.end()) {
+                    continue;
                 }
-                current[current_ind] = ascii; 
-                if (num_blanks > 0) {
+                current[current_ind] = temp;
+                //decrease number of blanks after replacing with a floating letter
+                if (num_blanks > 0 ){
                 num_blanks--;
                 }
+                used_letters.insert(temp);
+                floatingChars.pop();
                 wordle_helper(current, current_ind + 1, floatingChars, words, dict, num_blanks);
+                //after recursing, reinsert the - and increase num of blanks again, and reinsert the floating chars
                 current[current_ind] = '-';
                 num_blanks++;
-                ascii++;
+                floatingChars.push(temp);
+            }
+
+
+            //enter the second loop (entire alphabet) only if blanks are left in word
+            if (num_blanks > floatingChars.size()) {
+                while (ascii < 123) {
+                    //only insert letters that haven't been tried in floating characters group
+                    if (used_letters.find(ascii) != used_letters.end()) {
+                        ascii++;
+                        }
+                        current[current_ind] = ascii; 
+                        //decrease number of blanks after inserting
+                        if (num_blanks > 0) {
+                        num_blanks--;
+                        }
+                        //after recursing, reinsert dash, increase number of blanks, and increment ascii
+                        wordle_helper(current, current_ind + 1, floatingChars, words, dict, num_blanks);
+                        current[current_ind] = '-';
+                        num_blanks++;
+                        ascii++;
+                }
+            }
         }
-        }
-    }
-    current_ind++;
-    wordle_helper(current, current_ind, floatingChars, words, dict, num_blanks); 
+
+        //increment current index and recurse
+        current_ind++;
+        wordle_helper(current, current_ind, floatingChars, words, dict, num_blanks); 
     }
 
 }
